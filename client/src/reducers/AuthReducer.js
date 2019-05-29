46f5c35e529b46ac425feacda9d749f080b6ef12
Amar Teragundi <amarteragundi@gmail.com>
2019-05-29 16:53:02 +0530

    taking action types form types object

--- client/src/reducers/AuthReducer.js
+++ client/src/reducers/AuthReducer.js
@@ -1,13 +1,14 @@
 import actionTypes from "../actions/types";
 const INITIAL_STATE = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 };
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case actionTypes.SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payLoad };
     case actionTypes.SIGN_OUT:
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userId: null };
     default:
       return state;
   }
