2e575ad6e33e734754ae8e3ff1b427e21e9cc65d
Amar Teragundi <amarteragundi@gmail.com>
2019-05-29 14:16:14 +0530

    adding actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import actionTypes from "./types";
+
+export const SignIn = () => {
+  return {
+    type: actionTypes.SIGN_IN
+  };
+};
+
+export const SignOut = () => {
+  return {
+    type: actionTypes.SIGN_OUT
+  };
+};
