602fab15b42a4885a1c2f46245cc982970facf40
Amar Teragundi <amarteragundi@gmail.com>
2019-05-29 10:08:59 +0530

    committing base files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      hi
+      <BrowserRouter>
+        {/* <Route path="/" exact component={App} />
+        <Route path="/pagetwo" component={pageTwo} /> */}
+      </BrowserRouter>
+    </div>
+  );
+};
+
+const pageTwo = () => {
+  return <div>hi page two</div>;
 };
 
 export default App;
