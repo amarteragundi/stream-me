7ebd395b1532d9fa50fe131f11de92a5500e9c9c
Amar Teragundi <amarteragundi@gmail.com>
2019-05-29 10:18:26 +0530

    adding router to route to all pages

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
-      hi
       <BrowserRouter>
-        {/* <Route path="/" exact component={App} />
-        <Route path="/pagetwo" component={pageTwo} /> */}
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
       </BrowserRouter>
     </div>
   );
-};
-
-const pageTwo = () => {
-  return <div>hi page two</div>;
 };
 
 export default App;
