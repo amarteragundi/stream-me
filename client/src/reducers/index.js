d60f61bc2472d090287099aa0d791984b281ab67
Amar Teragundi <amarteragundi@gmail.com>
2019-05-31 10:30:27 +0530

    combining reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
 import { reducer as formReducer } from "redux-form";
+import streamReducer from "./StreamReducers";
 
 export default combineReducers({
   auth: AuthReducer,
-  form: formReducer
+  form: formReducer,
+  streams: streamReducer
 });
