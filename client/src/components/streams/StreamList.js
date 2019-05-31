1359a65b12ada33befdae83c0eb8d0688fd97f81
Amar Teragundi <amarteragundi@gmail.com>
2019-05-31 15:55:30 +0530

    display edit or delete for current user streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -6,6 +6,13 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.FetchStreams();
   }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return <div>Edit/Delete</div>;
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
@@ -15,6 +22,7 @@ class StreamList extends React.Component {
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
+          {this.renderAdmin(stream)}
         </div>
       );
     });
@@ -29,7 +37,10 @@ class StreamList extends React.Component {
   }
 }
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 export default connect(
   mapStateToProps,
