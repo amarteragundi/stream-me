7cb3e41254c0671b340e86a881abf83959608d98
Amar Teragundi <amarteragundi@gmail.com>
2019-05-31 09:20:30 +0530

    adding all actions required for the app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import actionTypes from "./types";
+import streams from "../apis/streams";
 
 export const SignIn = userId => {
   return {
@@ -11,4 +12,43 @@ export const SignOut = () => {
   return {
     type: actionTypes.SIGN_OUT
   };
+};
+
+export const CreateStream = formValues => async dispatch => {
+  const response = await streams.post("/streams", formValues);
+  dispatch({
+    type: actionTypes.CREATE_STREAM,
+    payload: response.data
+  });
+};
+
+export const FetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({
+    type: actionTypes.FETCH_STREAMS,
+    payload: response.data
+  });
+};
+
+export const FetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({
+    type: actionTypes.FETCH_STREAM,
+    payload: response.data
+  });
+};
+
+export const EditStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({
+    type: actionTypes.EDIT_STREAM,
+    payLoad: response.data
+  });
+};
+
+export const DeleteStream = id => async dispatch => {
+  dispatch({
+    type: actionTypes.DELETE_STREAM,
+    payload: id
+  });
 };
